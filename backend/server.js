16d0af0ba787488896abb7e7e50c04cfa0e9c086
Jadhav-Rutuja <105958121+Jadhav-Rutuja@users.noreply.github.com>
2026-02-09 00:51:25 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const app = express();
 // Middleware
 app.use(
   cors({
-    origin: "https://minisocialapp-4prn.onrender.com/api",
+    origin: "https://mini-social-app-l7xx.vercel.app",
     credentials: true,
   })
 );
@@ -32,5 +32,6 @@ mongoose.connect(process.env.MONGO_URI)
     );
   })
   .catch(err => console.log(err));
+
 
 
